8a26c908cd9e8e5af503279a3d8f53c06c6f2237
shizuku002 <k024g0037@g.neec.ac.jp>
2025-08-08 16:23:24 +0900

    AL3_02-08_8.8

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -45,23 +45,38 @@ public:
 	static inline const float kLimitFallSpeed = 0.5f;
 	static inline const float kJumpAcceleration = 2.0f;
 
+	// 速度加算
 	const KamataEngine::Vector3& GetVelocity() const { return velocity_; }
+	// マップチップSetter
 	void SetMapChipField(MapChipField* mapChipField) { mapChipField_ = mapChipField; }
-	// ①移動入力
-	void InputMove();
+
 	//キャラクターの当たり判定サイズ
 	static inline const float kWidth = 0.8f;
 	static inline const float kHeight = 0.8f;
+
+	// ①移動入力
+	void InputMove();
 	// ②マップ衝突判定
 	void CheckMapCollision(CollisionMapInfo& info);
 	// マップ衝突判定上
 	void CheckMapCollisionUp(CollisionMapInfo& info);
+	// マップ衝突判定下
+	void CheckMapCollisionDown(CollisionMapInfo& info);
+	// マップ衝突判定右
+	void CheckMapCollisionRight(CollisionMapInfo& info);
+	// マップ衝突判定左
+	void CheckMapCollisionLeft(CollisionMapInfo& info);
 	// ③判断結果を反映して移動させる
 	void CheckMapMove(const CollisionMapInfo& info);
 	// ④天井に接着している場合の処理
 	void CheckMapCeiling(const CollisionMapInfo& info);
+	// ⑤壁に接着している場合の処理
+	void CheckMapWall(const CollisionMapInfo& info);
+	// ⑥接地状態の切り替え
+	void CheckMapLanding(const CollisionMapInfo& info);
 	// ⑦旋回制御
 	void AnimateTurn();
+
 	// 角
 	enum Corner {
 		kRightBottom, // 右下
@@ -70,10 +85,18 @@ public:
 		kLeftTop,     //
 		kNumCorner    // 要素数
 	};
+
 	// 指定した角の座標計算
 	KamataEngine::Vector3 CornerPosition(const KamataEngine::Vector3& center, Corner corner);
+	
 	// 隙間
 	static inline const float kBlank = 0.1f;
+	// 着地時の速度減衰率
+	static inline const float kAttenuationLanding = 0.5f;
+	// 微小な数値
+	static inline const float kGroundSearchHeight = 0.1f;
+	// 壁の速度減衰率
+	static inline const float kAttenuationWall = 0.5f;
 
 public:
 	// 初期化
