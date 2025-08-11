6a9a67c9e772eea31b77c42005ddcaa021e7b9d7
shizuku002 <k024g0037@g.neec.ac.jp>
2025-08-11 10:51:10 +0900

    AL3_02-08_8.11

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -22,32 +22,32 @@ void GameScene::Initialize()
 	//自キャラの初期化
 	//player_->Initialize(modelPlayer_, &camera_);
 	skydome_->Initialize(modelSkydome_,& camera_);
-	//要素数
-	const uint32_t kNumBlockVirtical = 10;
-	const uint32_t kNumBlockHorizontal = 20;
-	//ブロック1個分の横幅
-	const float kBlockWidth = 2.0f;
-	const float kBlockHeight = 2.0f;
-	//要素数を変更する
-	worldTransformBlocks_.resize(kNumBlockVirtical);
-	for (uint32_t i = 0; i < kNumBlockVirtical; ++i)
-	{
-		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
-	}
-	//キューブの生成
-	for (uint32_t i = 0; i < kNumBlockVirtical; ++i)
-	{
-		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j)
-		{
-			if ((j + i) % 2 == 0)
-				continue;
+	////要素数
+	//const uint32_t kNumBlockVirtical = 10;
+	//const uint32_t kNumBlockHorizontal = 20;
+	////ブロック1個分の横幅
+	//const float kBlockWidth = 2.0f;
+	//const float kBlockHeight = 2.0f;
+	////要素数を変更する
+	//worldTransformBlocks_.resize(kNumBlockVirtical);
+	//for (uint32_t i = 0; i < kNumBlockVirtical; ++i)
+	//{
+	//	worldTransformBlocks_[i].resize(kNumBlockHorizontal);
+	//}
+	////キューブの生成
+	//for (uint32_t i = 0; i < kNumBlockVirtical; ++i)
+	//{
+	//	for (uint32_t j = 0; j < kNumBlockHorizontal; ++j)
+	//	{
+	//		if ((j + i) % 2 == 0)
+	//			continue;
 
-			worldTransformBlocks_[i][j] = new WorldTransform();
-			worldTransformBlocks_[i][j]->Initialize();
-			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
-			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
-		}
-	}
+	//		worldTransformBlocks_[i][j] = new WorldTransform();
+	//		worldTransformBlocks_[i][j]->Initialize();
+	//		worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
+	//		worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
+	//	}
+	//}
 	// デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
 	mapChipField_->LoadMapChipCsv("Resources/blocks.csv");
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -41,9 +41,9 @@ public:
 	static inline const float kTimeTurn = 0.9f;
 
 	bool onGround_ = true;
-	static inline const float kGravityAcceleration = 0.5f;
-	static inline const float kLimitFallSpeed = 0.5f;
-	static inline const float kJumpAcceleration = 2.0f;
+	static inline const float kGravityAcceleration = 0.1f;
+	static inline const float kLimitFallSpeed = 1.0f;
+	static inline const float kJumpAcceleration = 1.0f;
 
 	// 速度加算
 	const KamataEngine::Vector3& GetVelocity() const { return velocity_; }
